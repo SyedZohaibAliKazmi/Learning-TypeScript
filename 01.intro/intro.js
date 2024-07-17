76c986aca190caba6b6f011d4c255973fac65a14
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-07-17 20:31:11 +0500

    intro & install

---
+++ 01.intro/intro.js
@@ -0,0 +1,8 @@
+var Name1 = "Syed Zohaib Ali Kazmi";
+console.log(Name1);
+// let name = 95
+// console.log( name)
+var Username = "zohaib ali";
+var Username2 = " kazmi";
+var Username3 = Username + Username2;
+console.log(Username3);
---
+++ 01.intro/intro.ts
@@ -0,0 +1,9 @@
+let Name1 ="Syed Zohaib Ali Kazmi"
+console.log( Name1)
+
+// let name = 95
+// console.log( name)
+ const Username ="zohaib ali"
+ const Username2 =" kazmi"
+ const Username3 =Username + Username2
+ console.log( Username3)
